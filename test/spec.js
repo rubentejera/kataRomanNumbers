c6993c50dc406930d3d6e263f715b323f6224505
rubentejera <ruben.tejera@leanmind.es>
2018-05-22 16:27:27 +0100

    Red:converts number to Roman numerals

--- test/spec.js
+++ test/spec.js
@@ -8,6 +8,7 @@ describe("test de prueba", function () {
     it("converts numbers to Roman numerals", function () {
         expect(romanNumerals().fromNumber(1)).toBe("I");
         expect(romanNumerals().fromNumber(2)).toBe("II");
+        expect(romanNumerals().fromNumber(3)).toBe("III");
     });
 
 
