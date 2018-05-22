1b4bff7314dd53ead7986279c0965ed922864517
rubentejera <ruben.tejera@leanmind.es>
2018-05-22 16:26:11 +0100

    Green:converts number to Roman numerals

--- src/romanNumerals.js
+++ src/romanNumerals.js
@@ -1,10 +1,14 @@
 function romanNumerals() {
-    function fromNumber(){
-        return "I";
+    function fromNumber(number) {
+        if (number == 1) {
+            return "I";
+        }
+        return "II";
+
     }
 
 
-    return{
+    return {
         fromNumber,
     }
 }
