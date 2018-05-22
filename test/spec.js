fb59cc2aa68d727633d243344dedc1372d573245
rubentejera <ruben.tejera@leanmind.es>
2018-05-22 15:53:16 +0100

    Red:converts number to Roman numerals

--- test/spec.js
+++ test/spec.js
@@ -1,13 +1,12 @@
-const app = require("../src/main");
-const romanNumerals = require("../src/romanNumerals");
+const romanNumerals = require('../src/romanNumerals');
 
 describe("test de prueba", function () {
-    xit("verdadero es verdadero", function () {
+    it("verdadero es verdadero", function () {
         expect(true).toBeTruthy();
     });
 
     it("converts numbers to Roman numerals", function () {
-        expect(romanNumerals.fromNumber(1)).toBe("I");
+        expect(romanNumerals().fromNumber(1)).toBe("I");
     });
 
 
