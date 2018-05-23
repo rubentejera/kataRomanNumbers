3bf6e17e3bb812da785b00edc190f061f8d8412f
rubentejera <ruben.tejera@leanmind.es>
2018-05-23 14:10:57 +0100

    Modified romanNumerals() and Green:converts number to Roman numerals

--- src/romanNumerals.js
+++ src/romanNumerals.js
@@ -5,6 +5,16 @@ export default function romanNumerals() {
     };
 
     var numerals = [
+        numeral(1000,"M"),
+        numeral(900,"CM"),
+        numeral(500,"D"),
+        numeral(400,"CD"),
+        numeral(100,"C"),
+        numeral(90,"XC"),
+        numeral(50,"L"),
+        numeral(40,"XL"),
+        numeral(10,"X"),
+        numeral(5,"V"),
         numeral(4,"IV"),
         numeral(1,"I")
     ];
